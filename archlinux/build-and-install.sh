1d2c4dda119e136c44a1fced5ebdc495d3d96aa7
Benoit Pierre <benoit.pierre@gmail.com>
2015-09-26 01:03:30 +0200

    archlinux: update buid-and-install.sh
    - switch to bash
    - fix temporary PKGBUILD placement
    - take into account ~/.makepkg.conf

--- archlinux/build-and-install.sh
+++ archlinux/build-and-install.sh
@@ -1,19 +1,19 @@
-#! /bin/zsh
+#! /bin/bash
 
 set -x
 
-unset options
+[ -r "$HOME/.makepkg.conf" ] && . "$HOME/.makepkg.conf"
 
-cd "$0"(:h) &&
+cd "$(dirname "$0")" &&
 . ./PKGBUILD &&
 pkgver="$(git describe --tags --match='[0-9]*' | sed -n '/^[0-9][.0-9a-z-]\+$/{s/-/./g;p;Q0};Q1')" &&
 src="src/$pkgname-$pkgver" &&
 rm -rf src pkg &&
 mkdir -p "$src" &&
-sed "s/^pkgver=.*\$/pkgver=$pkgver/" ./PKGBUILD >src/PKGBUILD &&
-(cd "$OLDPWD" && git ls-files -z | xargs -0 cp --parents --target-directory="$OLDPWD/$src") &&
-export PACKAGER="`git config user.name` <`git config user.email`>" &&
-makepkg --noextract --force -p src/PKGBUILD &&
-rm -rf src pkg &&
-sudo pacman -U --noconfirm "$pkgname-$pkgver-$pkgrel-any.pkg.tar.xz"
+sed "s/^pkgver=.*\$/pkgver=$pkgver/" PKGBUILD >PKGBUILD.tmp &&
+(cd "$OLDPWD" && git ls-files -z | xargs -0 cp -a --no-dereference --parents --target-directory="$OLDPWD/$src") &&
+export PACKAGER="${PACKAGER:-`git config user.name` <`git config user.email`>}" &&
+makepkg --noextract --force -p PKGBUILD.tmp &&
+rm -rf src pkg PKGBUILD.tmp &&
+sudo pacman -U --noconfirm "$pkgname-$pkgver-$pkgrel-any${PKGEXT:-.pkg.tar.xz}"
 
